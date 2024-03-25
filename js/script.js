85dd2c1a75215f5dd853fb07951bde7757baeb30
Brenno Alencar <127119853+BrennoAlenkar@users.noreply.github.com>
2024-03-25 09:35:45 -0300

    Atualizei

--- js/script.js
+++ js/script.js
@@ -1,26 +1,35 @@
 const menu = document.querySelector('#menu');
 const cardBtn = document.querySelector('#card-btn');
 const cardModal = document.querySelector('#card-modal');
-const cartItemsContainer = document.querySelector('#card-items');
-const cartTotal = document.querySelector('#card-total');
+const cardItemsContainer = document.querySelector('#card-items');
+const cardTotal = document.querySelector('#card-total');
 const checkoutBtn = document.querySelector('#checkout-btn');
 const closeModalBtn = document.querySelector('#close-modal-btn');
-const cartCounter = document.querySelector('#cart-count');
+const cardCounter = document.querySelector('#cart-count');
 const addressInput = document.querySelector('#address');
 const addressWarn = document.querySelector('#address-warn');
-
-console.log(menu)
-console.log(cardBtn)
-console.log(cardModal)
-console.log(cartItemsContainer)
-console.log(cartTotal)
-console.log(checkoutBtn)
-console.log(closeModalBtn)
-console.log(cartCounter)
-console.log(addressInput)
-console.log(addressWarn)
-
 
 cardBtn.addEventListener('click', function(){
     cardModal.style.display = 'flex';
 })
+
+cardModal.addEventListener('click', function(event){
+    if(event.target === cardModal) {
+        cardModal.style.display = 'none'
+    }
+})
+
+closeModalBtn.addEventListener('click', function(){
+    cardModal.style.display = 'none'
+})
+
+menu.addEventListener('click', function(event){
+    let parentButton = event.target.closest('.add-to-card-btn')
+
+    if(parentButton) {
+        const name = parentButton.getAttribute("data-name")
+        const price = parentButton.getAttribute("data-price")
+    }
+})
+
+62 98101-2014
