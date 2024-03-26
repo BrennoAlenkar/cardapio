6dfd3235129e819c08bb1c96f235e7f356e0e8d5
Brenno Alencar <127119853+BrennoAlenkar@users.noreply.github.com>
2024-03-26 13:31:37 -0300

    Atualizei

--- js/script.js
+++ js/script.js
@@ -32,7 +32,7 @@ menu.addEventListener('click', function (event) {
     if (parentButton) {
         const name = parentButton.getAttribute("data-name")
         const price = parseFloat(parentButton.getAttribute("data-price"))
-        addToCart(name,price)
+        addToCart(name, price)
 
     }
 })
@@ -58,14 +58,14 @@ function updateCartModal() {
     cartItemsContainer.innerHTML = '';
     let total = 0;
 
-    cart.forEach(item =>{
+    cart.forEach(item => {
         const cartItemElement = document.createElement("div");
         cartItemElement.classList.add("flex", "Justify-between", "mb-4", "flex-col")
         cartItemElement.innerHTML = `
         <div class="flex items-center justify-between">
             <div>
             <p class="font-medium">${item.name}</p>
-            <p>Quantidade:${item.quantidade}</p>
+            <p>Quantidade: ${item.quantidade}</p>
             <p class="fonte-medium mt-2">R$${item.price.toFixed(2)}</p>
             </div>
 
@@ -89,8 +89,76 @@ function updateCartModal() {
 
 }
 
-cartItemsContainer.addEventListener('click', function(event){
-    if(event.target.classList.contains("remover-btn")){
+cartItemsContainer.addEventListener('click', function (event) {
+    if (event.target.classList.contains("remover-btn")) {
         const name = event.target.getAttribute("data-name")
+
+        removeItemCart(name);
     }
 })
+
+function removeItemCart(name) {
+    const index = cart.findIndex(item => item.name === name)
+
+    if(index !== -1) {
+       const item = cart[index]; 
+
+       if(item.quantidade >1) {
+        item.quantidade -= 1;
+        updateCartModal()
+        return;
+       }
+
+       cart.splice(index, 1);
+       updateCartModal()
+    }
+
+}
+
+
+addressInput.addEventListener("input", function(event) {
+    let inputValue = event.target.value;
+
+    if(inputValue !== "") {
+        addressInput.classList.remove("border-red-500")
+        addressWarn.classList.add("hidden")
+    }
+
+})
+
+checkoutBtn.addEventListener("click", function(){
+
+    const isOpen = checkAberto();
+    if(!isOpen) {
+        alert("RESTAURANTE FECHADO NO MOMENTO!")
+        return;
+    }
+
+    if(cart.length === 0) return;
+    if(addressInput.value === "") {
+        addressWarn.classList.remove("hidden")
+        addressInput.classList.add("border-red-500")
+        return;
+    }
+
+})
+
+function checkAberto() {
+    const data = new Date();
+    const hora = data.getHours();
+    return hora >= 18 && hora < 22;
+
+}
+
+const spanItem = document.querySelector("#date-span")
+const isOpen = checkAberto();
+
+
+if(isOpen){
+    spanItem.classList.remove("bg-red-500");
+    spanItem.classList.add("bg-green-600")
+} else {
+    spanItem.classList.remove("bg-green-600")
+    spanItem.classList.add("bg-red-500")
+    
+}
